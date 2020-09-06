867323b75f862643d69cb0bfbdc48b8431765bf3
TauVip <Abylai1393@gmail.com>
2020-09-06 22:03:40 +0600

    commit 1

---
+++ screens/HomeScreen.js
@@ -0,0 +1,125 @@
+import React from 'react'
+import { View, SectionList } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import styled from 'styled-components/native'
+
+import { Appointment, SectionTitle } from '../components'
+
+const DATA = [
+  {
+    title: '14 сентября',
+    data: [
+      {
+        time: '15:30',
+        active: true,
+        diagnosis: 'пульпит',
+        user: {
+          fullname: 'Вася Пупкин',
+          avatar: 'http://f4.24open.ru/SjgrfT1Nnp.jpg'
+        }
+      },
+      {
+        time: '18:00',
+        diagnosis: 'удаление зуба',
+        user: {
+          fullname: 'Иван Иванов',
+          avatar: 'https://i.ytimg.com/vi/tmW-9sFy9mw/maxresdefault.jpg'
+        }
+      },
+      {
+        time: '15:30',
+        diagnosis: 'пульпит',
+        user: {
+          fullname: 'Вася Пупкин',
+          avatar: 'http://f4.24open.ru/SjgrfT1Nnp.jpg'
+        }
+      },
+      {
+        time: '18:00',
+        diagnosis: 'удаление зуба',
+        user: {
+          fullname: 'Иван Иванов',
+          avatar: 'https://i.ytimg.com/vi/tmW-9sFy9mw/maxresdefault.jpg'
+        }
+      }
+    ]
+  },
+  {
+    title: '16 сентября',
+    data: [
+      {
+        time: '15:30',
+        diagnosis: 'пульпит',
+        user: {
+          fullname: 'Вася Пупкин',
+          avatar: 'http://f4.24open.ru/SjgrfT1Nnp.jpg'
+        }
+      },
+      {
+        time: '18:00',
+        diagnosis: 'удаление зуба',
+        user: {
+          fullname: 'Иван Иванов',
+          avatar: 'https://i.ytimg.com/vi/tmW-9sFy9mw/maxresdefault.jpg'
+        }
+      },
+      {
+        time: '15:30',
+        diagnosis: 'пульпит',
+        user: {
+          fullname: 'Вася Пупкин',
+          avatar: 'http://f4.24open.ru/SjgrfT1Nnp.jpg'
+        }
+      },
+      {
+        time: '18:00',
+        diagnosis: 'удаление зуба',
+        user: {
+          fullname: 'Иван Иванов',
+          avatar: 'https://i.ytimg.com/vi/tmW-9sFy9mw/maxresdefault.jpg'
+        }
+      }
+    ]
+  }
+]
+
+const HomeScreen = () => {
+  return (
+    <Container>
+      <SectionList
+        sections={DATA}
+        keyExtractor={(item, index) => index}
+        renderItem={({ item }) => <Appointment {...item} />}
+        renderSectionHeader={({ section: { title } }) => (
+          <SectionTitle>{title}</SectionTitle>
+        )}
+      />
+      <PlusButton>
+        <Ionicons name="ios-add" size={36} color="white" />
+      </PlusButton>
+    </Container>
+  )
+}
+
+const PlusButton = styled.TouchableOpacity`
+  align-items: center;
+  justify-content: center;
+  border-radius: 50px;
+  width: 64px;
+  height: 64px;
+  background-color: #2a86ff;
+  position: absolute;
+  right: 25px;
+  bottom: 25px;
+  shadow-color: #2a86ff;
+  shadow-opacity: 0.7;
+  shadow-radius: 3.5;
+  elevation: 4;
+`;
+
+const Container = styled.View`
+  flex: 1;
+  margin-top: 30px;
+`;
+
+export default HomeScreen
